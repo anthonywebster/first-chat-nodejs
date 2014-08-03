c282ce40f8820e2e4d39c6f5369c3b00a0c8bc9b
Anthony Webster <anthonyanroab@gmail.com>
2014-08-03 13:32:23 -0600

    test

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 3700;
 
-
 app.set('views',__dirname+'/tpl');
 app.set('view engine',"jade");
 app.engine('jade',require('jade').__express);
@@ -10,6 +9,7 @@ app.engine('jade',require('jade').__express);
 app.get("/",function(req,res){
 	res.render("page");
 });
+
 
 app.get('/user/:id', function(req, res){
   res.send('user ' + req.params.id);
