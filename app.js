722fc0e7fc6443ca9cefe1e2f066b0b502a68bf0
Anthony Webster <anthonyanroab@gmail.com>
2014-08-03 20:02:00 -0600

    modificando app

--- app.js
+++ app.js
@@ -21,9 +21,7 @@ var io = require('socket.io').listen(app.listen(port));
 
 io.sockets.on('connection',function(socket){
 	socket.emit('message',{message:'Welcome to the chat'});
-	socket.on('send',function(data){
-		io.sockets.emit('message',data);
-	});
+	
 });
 
 console.log("Listening on port"+port);
